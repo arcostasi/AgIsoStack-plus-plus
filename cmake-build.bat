f239ca9eef8ecbb6acc79cd7d6e6b99ab8621ccb
Anderson Costa <arcostasi@gmail.com>
2026-04-12 21:02:20 -0300

    build: improve local Windows CANlib scripts

--- cmake-build.bat
+++ cmake-build.bat
@@ -1,3 +1,56 @@
-cmake --build build -j16
-cd build
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+
+pushd "%~dp0"
+
+set "BUILD_CONFIG=%~1"
+if not defined BUILD_CONFIG (
+	set "BUILD_CONFIG=Debug"
+)
+
+echo Compilando configuracao "%BUILD_CONFIG%".
+cmake --build build --config "%BUILD_CONFIG%" -j16
+if errorlevel 1 (
+	popd
+	exit /b %errorlevel%
+)
+
+set "CANLIB_DLL="
+if exist "C:\Tools\Kvaser\Canlib\bin_x64\canlib32.dll" (
+	set "CANLIB_DLL=C:\Tools\Kvaser\Canlib\bin_x64\canlib32.dll"
+) else if exist "C:\Tools\Kvaser\Canlib\Bin\canlib32.dll" (
+	set "CANLIB_DLL=C:\Tools\Kvaser\Canlib\Bin\canlib32.dll"
+) else if exist "%~dp0hardware_integration\lib\Windows\canlib32.dll" (
+	set "CANLIB_DLL=%~dp0hardware_integration\lib\Windows\canlib32.dll"
+)
+
+if not defined CANLIB_DLL (
+	echo Aviso: canlib32.dll nao encontrada na instalacao da Kvaser nem no repositorio.
+	popd
+	exit /b 0
+)
+
+echo Usando canlib32.dll de "%CANLIB_DLL%".
+set /a COPIED_COUNT=0
+
+for /r "build" %%F in (*.exe) do (
+	set "TARGET_DIR=%%~dpF"
+	if /I "!TARGET_DIR:\CMakeFiles\=!"=="!TARGET_DIR!" (
+		if /I not "!TARGET_DIR:\%BUILD_CONFIG%\=!"=="!TARGET_DIR!" (
+			copy /Y "%CANLIB_DLL%" "%%~dpFcanlib32.dll" >nul
+			if not errorlevel 1 (
+				set /a COPIED_COUNT+=1
+				echo Copiada canlib32.dll para %%~dpF
+			)
+		)
+	)
+)
+
+if !COPIED_COUNT! EQU 0 (
+	echo Aviso: nenhum executavel foi encontrado em build para receber a DLL.
+)
+
+popd
+exit /b 0
+
 rem ctest
--- cmake-configure.bat
+++ cmake-configure.bat
@@ -1,1 +1,14 @@
-cmake -S . -B build -DCAN_DRIVER=WindowsCANlib -DBUILD_EXAMPLES=ON -DBUILD_TESTING=ON
+@echo off
+setlocal
+
+set "KVASER_CANLIB_LIB=C:\Tools\Kvaser\Canlib\Lib\x64\canlib32.lib"
+
+if exist "%KVASER_CANLIB_LIB%" (
+	echo Kvaser CANlib encontrada em "%KVASER_CANLIB_LIB%".
+) else (
+	echo Aviso: CANlib nao encontrada em "%KVASER_CANLIB_LIB%".
+	echo O projeto continuara usando a copia do repositorio em hardware_integration\lib\Windows.
+)
+
+cmake -S . -B build -DCAN_DRIVER=WindowsCANlib -DBUILD_EXAMPLES=ON -DBUILD_TESTING=ON -Wno-dev
+exit /b %errorlevel%
